4bd186a9ccaacf6c27eae18c1c3cf1569392d76a
Nicolas Thomazini <nicolasfrezarimt@gmail.com>
2024-09-25 16:46:06 -0300

    Initialized the repo with cookiecutter data science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A model training ML to predict what champion will be select to pick based a df with matches that champion was better in acording to the situations.',
+    author='Nicolas Thomazini',
+    license='MIT',
+)
